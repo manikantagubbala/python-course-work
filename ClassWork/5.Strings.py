7b5ae16d9300e5fb7a4c39db2f4eb60bb18980ad
BHUVANA MANIKANTA GUBBALA <100193959+manikantagubbala@users.noreply.github.com>
2025-07-05 12:33:25 +0000

    ClassWork

--- ClassWork/5.Strings.py
+++ ClassWork/5.Strings.py
@@ -1,6 +1,7 @@
 #5.Strings.py
 #5.1.OperationsOnStrings
 
+print("......OperationsOnStrings.......")
 s1='bhuvana '
 s2='Manikanta'
 print("Concatenation(+) : ", s1 + s2)             # Concatenation(+) :  bhuvana Manikanta
@@ -28,7 +29,7 @@ print("Membership : ", "bhuvana" in s)                 # Membership :  False
 #3. sorted() - Returns a sorted list of characters.
 #4. chr() / ord() - Converts between characters and their ASCII codes
 
-
+print("........Built-in String Functions..........")
 m='Looking beautiful'
 print("length : ",len(m))           #length :  17
 print("max : ", max(m))             #max :  u
@@ -46,6 +47,7 @@ print("Order : ", ord(' '))         #Order :  32
 #5.swapcase()
 #6.casefold()
 
+print(".........Case Conversion Methods.........")
 c='''i can improve mY SELF'''
 print("upper('A') : ", c.upper())                  #upper() :  I CAN IMPROVE MY SELF
 print("lower('a') : ", c.lower())                  #lower() :  i can improve my self
@@ -55,6 +57,7 @@ print("swapcase(A-a, a-A) : ", c.swapcase())       #swapcase() :  I CAN IMPROVE 
 
 
 #5.4.Search & Find Methods
+print("...........Search & Find Methods..............")
 f="adityaMandala"
 print("find : ", f.find('a'))           #find :  0
 print("rfind : ", f.rfind('a'))         #rfind :  12
@@ -67,6 +70,7 @@ print("count : ", f.count('a'))         #count : 5
 
 #5.5.Alignment & Formatting Methods
 
+print(".....Alignment & Formatting Methods......")
 print("center : ", f.center(20,"*"))         #center :  ***adityaMandala****
 print("ljust : ", f.ljust(16,"*"))           #ljust :  adityaMandala***
 print("rjust : ", f.rjust(16,"*"))           #rjust :  ***adityaMandala
@@ -76,6 +80,7 @@ print("zfill : ", '8978411298'.zfill(16))    #zfill :  0000008978411298
 
 #5.6.String Testing Methods (Boolean Results)
 
+print("..............String Testing Methods (Boolean Results).........")
 g='Manikanta12324'
 print("startswith(sub) : ", f.startswith('d'))          #startswith(sub) :  False
 print("startswith(sub) : ", f.startswith('a'))          #startswith(sub) :  True
@@ -91,6 +96,7 @@ print("istitle : ", f.istitle())                        #istitle :  False
 print("isidentifier : ", f.isidentifier())              #isidentifier :  True
 print("istitle : ", g.istitle())                        #istitle :  True
 
+print("................")
 print("         isdecimal()  ")
 print("isdecimal() : ", '123'.isdecimal())            #isdecimal() :  True
 print("isdecimal() : ", '123.67'.isdecimal())         #isdecimal() :  False
@@ -101,6 +107,7 @@ print("isdecimal() : ", '²'.isdecimal())              #isdecimal() :  False
 print("isdecimal() : ", '⅓'.isdecimal())              #isdecimal() :  False
 print("isdecimal() : ", '五'.isdecimal())             #isdecimal() :  False
 
+print("................")
 print("         isdigit()  ")
 print("isdigit() : ", '123'.isdigit())              #isdigit() :  True
 print("isdigit() : ", '123.67'.isdigit())           #isdigit() :  False
@@ -111,6 +118,7 @@ print("isdigit() : ", '²'.isdigit())                #isdigit() :  True
 print("isdigit() : ", '⅓'.isdigit())                #isdigit() :  False
 print("isdigit() : ", '五'.isdigit())               #isdigit() :  False
 
+print("................")
 print("         isnumeric()  ")
 print("isnumeric() : ", '123'.isnumeric())          #isnumeric() :  True
 print("isnumeric() : ", '123.67'.isnumeric())       #isnumeric() :  False
@@ -123,15 +131,42 @@ print("isnumeric() : ", '五'.isnumeric())           #isnumeric() :  True
 
 #5.7.Replace & Modify Methods
 
-print("replace(old,new) : ", f.replace('a', '@'))
-print("replace(old,new) : ", f.replace('ad', '@%'))
-print("maketrans() : ", f.maketrans('a', '@'))
-print(ord('a'))
-print(ord('@'))
-print("translate(table) : ", f.translate(str.maketrans('a', '@')))
-print("translate(table) : ", f.translate('str'.maketrans('ad', '@%')))
+print(".................Replace & Modify Methods...........")
+
+print("replace(old,new) : ", f.replace('a', '@'))                           #replace(old,new) :  @dity@M@nd@l@
+print("replace(old,new) : ", f.replace('ad', '@%'))                         #replace(old,new) :  @%ityaMandala    it has to change substring                           
+print("maketrans() : ", f.maketrans('a', '@'))                              #maketrans() :  {97: 64}
+print(ord('a'))                                                             #97
+print(ord('@'))                                                             #64
+print("translate(table) : ", f.translate(str.maketrans('a', '@')))          #translate(table) :  @dity@M@nd@l@
+print("translate(table) : ", f.translate(str.maketrans('ad', '@%')))        #translate(table) :  @%ity@M@n%@l@     it has to change c
 
 
+#5.8.Whitespace & Trimming Methods
+print("..........Whitespace & Trimming Methods.......")
+m='   manikanta '
+print("strip() :",m.strip())                            #strip() : manikanta
+print("lstrip() :",m.lstrip())                          #lstrip() : manikanta 
+print("rstrip() :",m.rstrip())                          #rstrip() :    manikanta
+
+#5.9.Splitting & Joining Methods
+#partition() and rpartition() both return a 3-part tuple.
+m='manikanta bhuvana gubbala'
+n='helloworld'
+print("........Splitting & Joining Methods............")                           
+print("split() : ", m.split())                                                     #split() :  ['manikanta', 'bhuvana', 'gubbala']
+print("rsplit() : ", m.rsplit())                                                   #rsplit() :  ['manikanta', 'bhuvana', 'gubbala']
+print("split() : ", m.split(" ",1))                                                #split() :  ['manikanta', 'bhuvana gubbala']
+print("rsplit() : ", m.rsplit(" ",1))                                              #rsplit() :  ['manikanta bhuvana', 'gubbala']
+print("splitlines() : ", "Hello\nworld\ni\nam\nManikanta".splitlines())            #splitlines() :  ['Hello', 'world', 'i', 'am', 'Manikanta']
+print("join() : ", " ".join(m))                                                    #join() :  m a n i k a n t a   b h u v a n a   g u b b a l a
+print("join() : ", " ".join(['manikanta',"bhuvana",'''gubbala''']))                #join() :  manikanta bhuvana gubbala
+print(n[0:5]+" "+n[5:10])                                                          #hello world
+print(n[1:10:2])                                                                   #elwrd
+print("partition() : ", m.partition(" "))                                          #partition() :  ('manikanta', ' ', 'bhuvana gubbala')
+print("rapartition() : ", m.rpartition(" "))                                       #rapartition() :  ('manikanta bhuvana', ' ', 'gubbala')
+print("partition() : ", "hello hi mani aditya eswar".partition(" "))               #partition() :  ('hello', ' ', 'hi mani aditya eswar')
 
 
-
+#5.10.Encoding & Decoding Methods
+print("..........Encoding & Decoding Methods.........")
---
+++ ClassWork/6.Lists.py
@@ -0,0 +1,12 @@
+#6.Lists.py
+
+print("............Creating Lists.............")
+l=[]
+l=[1,2,3,4,5]
+print(l)
+print(list())
+l.append('apple')
+print(l)
+
+print("...........List with Nested Lists..........")
+l=[[1,2,3], {4,5,6}, (7,8,9), True, [1,'apple']]
