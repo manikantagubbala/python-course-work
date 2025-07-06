c65fd0a3770c69fa717cf5f03f57944a87878519
BHUVANA MANIKANTA GUBBALA <100193959+manikantagubbala@users.noreply.github.com>
2025-07-06 06:00:59 +0000

    PracticeWork

---
+++ PracticeWork/day3.py
@@ -0,0 +1,16 @@
+fname="suryaprakasharao"
+print(len(fname))                                   #16
+print("Slicing- var[] : ", fname[:16])              #Slicing- var[] :  suryaprakasharao
+print("Slicing- var[] : ", fname[:])                #Slicing- var[] :  suryaprakasharao
+print("Slicing- var[] : ", fname)                   #Slicing- var[] :  suryaprakasharao
+print("Slicing- var[] : ", fname[5:16])             #Slicing- var[] :  prakasharao
+print("Slicing- var[] : ", fname[1:16:2])           #Slicing- var[] :  uypaahro
+print("Slicing- var[] : ", fname[0:16:2])           #Slicing- var[] :  srarksaa
+print("Slicing- var[] : ", fname[0:16:4])           #Slicing- var[] :  saka
+print("Slicing- var[] : ", fname[1:16:3])           #Slicing- var[] :  uaasr
+print("Slicing- var[] : ", fname[1:16:5])           #Slicing- var[] :  urh
+
+
+
+
+
