1996e40913d7dcdc80dacf15abcbeba56df83a88
BHUVANA MANIKANTA GUBBALA <100193959+manikantagubbala@users.noreply.github.com>
2025-07-07 05:31:43 +0000

    PracticeWork

--- PracticeWork/day3.py
+++ PracticeWork/day3.py
@@ -10,7 +10,22 @@ print("Slicing- var[] : ", fname[0:16:4])           #Slicing- var[] :  saka
 print("Slicing- var[] : ", fname[1:16:3])           #Slicing- var[] :  uaasr
 print("Slicing- var[] : ", fname[1:16:5])           #Slicing- var[] :  urh
 
+a='HackerRank.com presents "Pythonist 2" '
+print(a.swapcase())
 
+b='this is a string'
+c=b.split(" ")
+print("-".join(c))
+print(c)
 
+fname="Ross"
+lname="Taylor"
+s="Hello first last ! You just delved into python."
 
-
+d="abracadabra"
+#print(d[:4] + "k" + d[5:])
+#print(d)
+e=list(d)
+e[4]
+s=" ".join(e)
+print(s)
