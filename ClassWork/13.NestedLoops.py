a43077f41b89f53c0ac7e5065a0c9a3b2f605ff3
manikantagubbala <bmanikanta1357@gmail.com>
2025-07-17 18:33:32 +0530

    ClassWork

--- ClassWork/13.NestedLoops.py
+++ ClassWork/13.NestedLoops.py
@@ -35,6 +35,77 @@ for i in range(5):                                  #           *
         print("*",end=" ")                          #   * * * * *
     print()
 
+print("6".center(15," "))
 
+for i in range(5):                                  #   * * * * *  
+    for j in range(i):                              #     * * * *  
+        print(" ",end=" ")                          #       * * *
+    for k in range(5-i):                            #         * *
+        print("*",end=" ")                          #           *
+    print()
+
+
+
+print("7".center(15," "))
+
+for i in range(5):                                  #   *    
+    for j in range(i+1):                            #   * *    
+        print("*",end=" ")                          #   * * *    
+    print()                                         #   * * * *    
+                                                    #   * * * * *
+
+print("8".center(15," "))
+
+#n=int(input("Enter number : "))                    
+for i in range(9):                                  #   *      
+    if i<9/2:                                       #   * *     
+        for j in range(i+1):                        #   * * *     
+            print("*",end=" ")                      #   * * * *     
+        print()                                     #   * * * * *     
+    else:                                           #   * * * *     
+        for k in range(9-i):                        #   * * *     
+            print("*",end=" ")                      #   * *     
+        print()                                     #   *
+
+
+print("9".center(15," "))
+
+n=int(input("Enter num : "))
+m=n//2
+for i in range(n):                                  #       *          
+    if i<n/2 and n%2==0:                            #     * *           
+        for j in range(m-i-1):                      #   * * *           
+            print(" ",end=" ")                      #   * * *           
+        for k in range(i+1):                        #     * *           
+            print("*",end=" ")                      #       *
+        print()
+
+    elif i<n/2 and n%2!=0:                          #       * 
+        for j in range(m+1-i-1):                    #     * *    
+            print(" ",end=" ")                      #   * * *
+        for k in range(i+1):                        #     * *
+            print("*",end=" ")                      #       *
+        print()
+                                            
+    else:                                    
+        for j in range(i-m):                
+            print(" ",end=" ")                            
+        for k in range(n-i):                              
+            print("*",end=" ")
+        print()
+
+'''
+        * 
+      * *
+    * * *
+  * * * *
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+
+'''
 
 
