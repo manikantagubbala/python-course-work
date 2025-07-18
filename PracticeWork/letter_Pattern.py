d1da8c90329c6e0f016c2852862fd4d8678f364e
manikantagubbala <bmanikanta1357@gmail.com>
2025-07-18 22:56:13 +0530

    PracticeWork

--- PracticeWork/letter_Pattern.py
+++ PracticeWork/letter_Pattern.py
@@ -73,7 +73,7 @@ print("Letter-'S'".center(25," "))
 
 for i in range(n):
     for j in range(n):
-        if i==0 or i==n-1 or i==n//2 or (i<n//2 and j==0) or (i>n//2 and j==n-1):
+        if (i==0 and j!=0) or i==n-1 or i==n//2 or (i<n//2 and j==0 and i!=0) or (i>n//2 and j==n-1):
             print("*",end=" ")
         else:
             print(" ",end=" ")
@@ -94,7 +94,7 @@ print("Letter-'Q'".center(25," "))
 
 for i in range(n):
     for j in range(n):
-        if (i<n-2 and i==0) or (j<n-2 and j==0):
+        if i==n-2 or j==n-2 or i==0 or j==0 or (i==0 and j==n-1) :
             print("*",end=" ")
         else:
             print(" ",end=" ")
