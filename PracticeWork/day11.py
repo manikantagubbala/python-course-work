f3f6da286f6fe0dbd59cbb703fa70947a4585609
manikantagubbala <bmanikanta1357@gmail.com>
2025-07-19 17:06:49 +0530

    PracticeWork

---
+++ PracticeWork/day11.py
@@ -0,0 +1,7 @@
+print("IsPrime : ".center(20," "))
+
+for i in range(2,int(50 ** 0.5) + 1):
+    if 50%2==0 :
+        print("Np")
+else:
+    print("P")
--- PracticeWork/letter_Pattern.py
+++ PracticeWork/letter_Pattern.py
@@ -1,6 +1,8 @@
-print("Letter-'Z'".center(25," "))
 
 n=int(input("Enter the size : "))
+
+print("Letter-'Z'".center(25," "))
+
 for i in range(n):
     for j in range(n):
         if i==0 or i==n-1 or i+j==n-1:
@@ -73,11 +75,12 @@ print("Letter-'S'".center(25," "))
 
 for i in range(n):
     for j in range(n):
-        if (i==0 and j!=0) or i==n-1 or i==n//2 or (i<n//2 and j==0 and i!=0) or (i>n//2 and j==n-1):
+        if (i==0 and j!=0) or (i==n-1 and j!=n-1) or i==n//2 or (i<n//2 and j==0 and i!=0) or (i>n//2 and j==n-1 and i!=n-1):
             print("*",end=" ")
         else:
             print(" ",end=" ")
     print()
+
 
 print("Letter-'R'".center(25," "))
 
@@ -94,8 +97,184 @@ print("Letter-'Q'".center(25," "))
 
 for i in range(n):
     for j in range(n):
-        if i==n-2 or j==n-2 or i==0 or j==0 or (i==0 and j==n-1) :
+        if (i==0 and j!=n-1) or (j==0 and i!=n-1) or (j==n-2 and i!=n-1) or (i==n-2 and j!=n-1) or (i>=n//2 and i==j):
             print("*",end=" ")
         else:
             print(" ",end=" ")
     print()
+
+print("Letter-'P'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n//2 or (i<n//2 and j==n-1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+
+    print()
+
+
+print("Letter-'O'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'N'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if j==0 or j==n-1 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'M'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if j==0 or j==n-1 or (i<=n//2 and (i==j or i+j==n-1)):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'L'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if j==0 or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'K'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if j==n//2-1 or (j>n//2 and i+j==n-1) or (i>=n//2 and i==j):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+print("Letter-'J'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==n//2 or (i>n//2 and i-j==n//2+1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+print("Letter-'I'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+print("Letter-'H'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if j==0 or j==n-1 or i==n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'5'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or (i>=n//2 and j==n-1) or (j>=n//2 and i==n//2):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+print("Letter-'F'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or (j<=n//2 and i==n//2) or j==0:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'E'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or i==n//2 or j==0 :
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'D'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if (i==0 and j!=n-1) or j==0 or (i==n-1 and j!=n-1) or (j==n-1 and i!=0 and i!=n-1) :
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+print("Letter-'C'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("Letter-'B'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if( i==0 and j!=n-1) or j==0 or (i==n-1 and j!=n-1) or (j==n-1 and i!=0 and i!=n//2 and i!=n-1) or (i==n//2 and j!=n-1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+print("Letter-'A'".center(25," "))
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n//2 or j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+
+
+
+
+
+
