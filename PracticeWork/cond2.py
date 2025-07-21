ae2c1fea7ef88020779547072b0b09e69916019c
manikantagubbala <bmanikanta1357@gmail.com>
2025-07-21 22:37:46 +0530

    PracticeWork

--- PracticeWork/cond2.py
+++ PracticeWork/cond2.py
@@ -92,7 +92,7 @@ if n>=50 and n<=100 and n%5==0:
 else:
     print("Not In range and divisible by 5")
 
-'''
+    
 
 print("Validate if a password length is strong (8 or more characters)")
 password=input("Enter your password: ")
@@ -100,3 +100,79 @@ if len(password)>=8:
     print("Strong password")
 else:
     print("weak password")
+
+
+
+print("Check if sum of two numbers is even")
+a=list(map(int,input("Enter numbers: ").split()))
+if sum(a)%2==0:
+    print("Sum is Even")
+else:
+    print("Sum is Odd")
+    
+
+
+print("Check if the character is a special symbol (!, @, #, etc.)")
+c=input("Enter a character: ")
+if c.isalnum():
+    print(f'{c} is not a Special Character')
+else:
+    print(f'{c} is a Special character')
+
+
+
+print("Check if temperature is cold (<15°C), moderate (15–30°C), or hot (>30°C)")
+temp=int(input("Enter Room Temperature: "))
+if temp<=15:
+    print(f'Cold')
+elif temp>15 and temp<=30:
+    print("Moderate")
+else:
+    print("Hot")
+
+
+print("Check if a number lies outside the range 10 to 50")
+n=int(input("Enter Number: "))
+if n>=10 and n<=50:
+    print(f'{n} is inside the range')
+else:
+    print(f'{n} is outside the range')
+    
+
+    
+print("Compare two ages and determine who is older or if same age")
+n=int(input("Enter Number: "))
+m=int(input("Enter Number: "))
+if n>m:
+    print(f'{n} is older')
+else:
+    print(f'{m} is older')
+    
+
+
+print("Check if an angle is acute, right, or obtuse")
+n=int(input("Enter an Angle: "))
+if n<90:
+    print(f'{n} is Acute angle')
+elif n==90:
+    print(f"{n} is Right angle")
+elif n>90 and n<180:
+    print(f'{n} is obtuse angle')
+elif n==180:
+    print(f'{n} is Straight angle')
+else:
+    print(f"{n} is not an angle")
+
+'''
+
+print("Check if number is a perfect square (basic method)")
+n=int(input("Enter a Square number: "))
+is_square=False
+for i in range(1,n):
+    if i*i==n:
+        is_square=True
+        break
+if is_square:
+    print(f"{n} is perfect square")
+else:
+    print(f"{n} is not perfect Square")
