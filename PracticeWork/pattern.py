e0377f7cfc36a3b5e370da155e12708a29375622
manikantagubbala <bmanikanta1357@gmail.com>
2025-07-22 12:32:10 +0530

    PracticeWork

---
+++ PracticeWork/pattern.py
@@ -0,0 +1,40 @@
+print("Pattern".center(20,"*"))
+for i in range(5):
+    for j in range(i+1):
+        print('*',end=" ")
+    print()
+
+print()
+
+for i in range(5):
+    for j in range(5-i):
+        print('*',end=" ")
+    print()
+
+print()
+for i in range(5):
+    for j in range(i):
+        print(' ',end=" ")
+    for k in range(5-i):
+        print("*",end=" ")
+    print()
+
+print()
+for i in range(5):
+    for j in range(5-i-1):
+        print(" ",end=" ")
+    for k in range(i+1):
+        print('*',end=" ")
+    print()
+
+print()
+n=int(input("Enter size: "))
+for i in range(n):
+    if i<=n//2:
+        for j in range(i+1):
+            print('*',end=" ")
+        print()
+    else:
+        for k in range(n-i):
+            print('*',end=" ")
+        print()
