5c21d971109c2f1f99b243c8bfb3ce00f18bf3d1
manikantagubbala <bmanikanta1357@gmail.com>
2025-07-24 16:58:40 +0530

    PracticeWork

--- PracticeWork/day1.py
+++ PracticeWork/day1.py
@@ -1,1 +1,32 @@
-print("Hello")
+
+'''
+print("Streak")
+
+n=list(map(int,input("Enter steaks : ").split()))
+cs=0
+ls=0
+for i in n:
+    if i==1:
+        cs+=1
+        if cs>ls:
+            ls=cs
+    else:
+        cs=0
+
+print(ls)
+print(cs)
+
+'''
+
+print("ATM")
+stored_pin='1234'
+n=int(input("Enter size: "))
+for i in range(n):
+     pin=input("Enter your PIN: ")
+     if pin==stored_pin:
+          print("Login Successfull")
+          break
+     else:
+          print("PIN Incorrect")
+else:
+     print("ATM Blocked")
