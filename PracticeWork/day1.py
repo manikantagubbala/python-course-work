10c86fefa56e15cb161f473582512665fe13c013
manikantagubbala <bmanikanta1357@gmail.com>
2025-08-04 20:02:43 +0530

    PracticeWork

--- PracticeWork/day1.py
+++ PracticeWork/day1.py
@@ -65,7 +65,6 @@ for i in a:
         d[i]=a.count(i)
     
 print(d)
-'''
 
 
 def dic_ount(text):
@@ -76,3 +75,113 @@ def dic_ount(text):
     return h
 text=input("Enter strings: ").split()
 print(dic_ount(text))
+
+'''
+
+def is_anagram(a,b):
+    for i in a :
+        if i in b:
+            return True
+    else:
+        return False
+
+
+a='apple'
+b='ple'
+print(is_anagram(a,b))
+
+a=[1,2,3,4,5,6]
+even=list(filter(lambda i:i%2==0,a))
+print(even)
+
+def email(f,l,d):
+    mail=f+"."+l+"."+d
+    return mail
+f='mani'
+l='kanta'
+d='com'
+print(email(f,l,d))
+
+print()
+'''
+a=[[1,2],[3,4],[8]]
+b=[]
+c=[]
+b.append(a[0][0])
+b.append(a[0][1])
+b.append(a[1][0])
+b.append(a[1][1])
+print(b)
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        c.append(a[i][j])
+
+print(c)
+'''
+
+#a=[[1],[2,3],[4]]
+#print(a[0][0])
+#print(len(a[2]))
+'''
+def is_anagram(s1,s2):
+    if len(s1)>len(s2) or len(s1)<len(s2):
+        return False
+    else:
+        return True
+s1,s2=input("Enter: ").split()
+print(is_anagram(s1,s2))
+
+
+def f_matrix(matrix):
+    a=[]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            a.append(matrix[i][j])
+    return a
+matrix=eval(input())
+print(f_matrix(matrix))
+
+
+def f_matrix(m):
+    a=[]
+    for i in m:
+        for j in i:
+            a.append(j)
+    return a
+m=eval(input())
+print(f_matrix(m))
+
+k=[]
+l=[[1],[2,3],[4]]
+for i in l:
+    for j in i:
+        k.append(j)
+print(k)
+
+
+def sum_of_digits(n):
+    if n==0:
+        return n
+    return n%10 + sum_of_digits(n//10)
+print("sumOfDigits: ",sum_of_digits(234))
+
+def power(bases,exp):
+    if exp<=0:
+        return 1
+    return bases**exp
+bases=int(input("Enter num: "))
+exp=int(input("Enter num: "))
+print(power(bases,exp))
+
+'''
+
+def is_anagram(s1,s2):
+    s1=s1.lower()
+    s2=s2.lower()
+    if sorted(s1)==sorted(s2):
+        return True
+    else:
+        return False
+s1,s2=input("Enter values: ").split()
+print(is_anagram(s1,s2))
