ce7ecf620d98e4bfdf545effdb8df25072339199
manikantagubbala <bmanikanta1357@gmail.com>
2025-08-05 23:08:01 +0530

    Real_World_Programs

--- Real_World_Programs/ATM.py
+++ Real_World_Programs/ATM.py
@@ -2,4 +2,19 @@
 import ATM_Module as atm
 
 atm.Welcome()
-atm.Login()
+
+if atm.Login():
+    while True:
+        atm.Menu()
+        option=input("Enter your choice in Menu: ").upper()
+        if option=='C':
+            atm.check_balance()
+        elif option=='D':
+            atm.deposit()
+        elif option=='W':
+            atm.withdraw()
+        elif option=="M":
+            atm.Mini_Statement()
+        elif option=="E":
+            print("Thankyou".center(50,"_"))
+            break
--- Real_World_Programs/ATM_Module.py
+++ Real_World_Programs/ATM_Module.py
@@ -13,12 +13,49 @@ def Welcome():
     print()
 
 def Login():
+    global account_num
     account_num=int(input("Enter Account Number: "))
+    global pin
     pin=int(input("Enter your pin: "))
     if account_num in data:
         if data[account_num]["pin"]==pin:
             print("Login Successful")
+            return True
         else:
             print("Invalid PIN")
+            return False
     else:
         print("Invalid Account Number")
+        return False
+
+def Menu():
+    print()
+    print("[c]heckBalnace")
+    print("[D]eposit")
+    print("[W]ithdraw")
+    print("[M]ini Statement")
+    print("[E]xit")
+
+def check_balance():
+    if account_num in data:
+        print(f"Current Balance: {data[account_num]["balance"]}")
+
+def deposit():
+    if account_num in data:
+        amount=int(input("Enter Deposit amount: "))
+        data[account_num]["balance"]+=amount
+        data[account_num]["history"].append(f"Deposit sucessfull Rs.{amount}")
+
+def withdraw():
+    if account_num in data:
+        amount=int(input("Enter Withdraw amount: "))
+        if amount<=data[account_num]["balance"]:
+            data[account_num]["balance"]-=amount
+            data[account_num]["history"].append(f"Withdraw successfull Rs.{amount}")
+
+def Mini_Statement():
+    if account_num in data:
+        print("Transactions".center(40,"-"))
+        for i in data[account_num]["history"]:
+            print(i)
+        print("End of the Transactions".center(40,"-"))
