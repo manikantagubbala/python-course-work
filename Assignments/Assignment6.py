31f9a4d0c696b5f511a75ec5d0d74c0d9c3d97f7
manikantagubbala <bmanikanta1357@gmail.com>
2025-09-12 12:56:40 +0530

    Assignments

--- Assignments/Assignment6.py
+++ Assignments/Assignment6.py
@@ -1,80 +1,30 @@
-# Matplotlibary.py
+import pandas as pd
 import matplotlib.pyplot as plt
 
+# Read CSV file
+df = pd.read_csv('social-media.csv')
+
+# Check the first few rows
+print(df.head())
+
+# Extract columns (make sure names match your CSV exactly)
+ids = df["UserId"]
+countries = df["Country"]
+
+
+# Plot bar chart
+plt.bar(countries, ids)
+plt.xlabel("Name of the Country")
+plt.ylabel("User Id")
+plt.title("Users by Country")
+plt.show()
+
+
 # Line Plot
-
-hours = [1,2,3,4,5,6,7,8]
-visitors = [50,60,55,70,90,120,110,130]
-
-plt.plot(hours, visitors,
-         color = "green",
-         marker = "o",                      # shape round(o), square(s), triangle(^)
-         linestyle = "--",
-         linewidth = 2,                     # line thickness
-         label = "Visitors",                # top of the graph, plt.legend is mand*
-         alpha = 1,                         # color brightness
-         )                         
-
-plt.title("Website Visitors Per Hour")
-plt.xlabel("Hour of Day")
-plt.ylabel("Number of Visitors")
-plt.legend()
-plt.grid(True)
-plt.show()
-
-#  Bar Graph
-
-day = ["Sun", "Mon", 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-hours = [6, 4, 5, 4, 4, 7, 8]
-
-plt.bar(day, hours,
-        color = "green",
-        alpha = 0.5,
-        edgecolor = "black",
-        label = "Time"
-        )
-
-plt.title("Mobile Screen Time Per Week")
-plt.xlabel("Day of the Week")
-plt.ylabel("Number of Hours")
-plt.legend()
+plt.plot(countries, ids)
+plt.xlabel("Name of the Country")
+plt.ylabel("User Id")
+plt.title("Users by Country")
 plt.show()
 
 
-# Histogram
-
-marks = [45,55,60,72,68,80,90,55,67,70,
-         85,40,60,75,78,82,95,50,62,65,
-         58,73,77,69,71,88,92,55,63,59
-         ]
-
-plt.hist(marks,
-         bins = 15,
-         color= "red",
-         edgecolor="black",
-         label="marks",
-         histtype= "step",
-#         density= "True"
-         )
-
-plt.title("Distribution of Student Marks")
-plt.xlabel("Marks Range")
-plt.ylabel("Number of Students")
-plt.legend()
-plt.show()
-
-
-# Pie Chart
-
-hours = [1,2,3,5]
-apps = ["Whatsapp", "Youtube", "Instagram", "Chrome"]
-color = ["green", 'red', 'GOLD', 'Skyblue']
-
-plt.pie(hours,labels=apps,                      
-        colors=color, 
-        startangle=90,                          # pie chart angle st with 90
-        explode=[0.1,0,0,0]                     # pick the piece
-        )
-
-plt.title("Most Used Apps in Mobile")
-plt.show()
